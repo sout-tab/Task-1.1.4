b0eeb5df2f19d7efee1f731c352d0f764a9636c3
yuliy-smrnv <yuliy.smrnv@gmail.com>
2022-10-07 22:15:47 +0300

    Init commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,22 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+        userService.saveUser("Person1", "Personov1", (byte) 18);
+        userService.saveUser("Person2", "Personov2", (byte) 20);
+        userService.saveUser("Person3", "Personov3", (byte) 22);
+        userService.saveUser("Person4", "Personov4", (byte) 24);
+
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,18 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+    private static final String url = "jdbc:postgresql://localhost:5432/IT-mentor";
+    private static final String user = "postgres";
+    private static final String password = "Tilita60";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
 }
+
